da531178f11783c37c612dedc9b6c95c6ad515b7
Vincente <vba16@sfu.ca>
2023-02-23 14:00:03 -0800

    Modified list data into a table modifying html code

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ function addItem(){
     let item = document.getElementById("new_item");
     let date = document.getElementById("date_picker");
     allItems.push({title: item.value, dueDate: date.value});
-    let element = document.createElement('div');
+    let table = document.getElementById("table_list");
+    let new_row = document.createElement('tr');
+    new_row.setAttribute("id", "row_item");
+    let element = document.createElement('td');
     element.setAttribute("id", "item_in_list");
     element.innerText = item.value;
-    let element_date = document.createElement('div');
+    let element_date = document.createElement('td');
     element_date.setAttribute("id", "date_in_list");
     element_date.innerText = date.value;
-    let to_do_list = document.getElementById('todo_list');
-    to_do_list.appendChild(element);
-    let to_do_list_date = document.getElementById('todo_list_date');
-    to_do_list_date.appendChild(element_date);
-    document.getElementById('new_item').value = "";
+    new_row.appendChild(element);
+    new_row.appendChild(element_date);
+    table.appendChild(new_row);
 
+
+    //Gets rid of text in the input field
+    document.getElementById('new_item').value = "";
+    document.getElementById("date_picker").value = null;
 
 
 
