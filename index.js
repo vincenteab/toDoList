652af26d59dd52a5275ccbcff550b66e928fa463
Vincente <vba16@sfu.ca>
2023-02-22 22:20:19 -0800

    aligned items in list with the input text box

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-let allItems = [];
+let allItems = [{
+    title: null,
+    dueDate: null
+        }];
 
 function addItem(){
     let item = document.getElementById("new_item");
-    allItems.push(item.value);
+    let date = document.getElementById("date_picker");
+    allItems.push({title: item.value, dueDate: date.value});
     let element = document.createElement('div');
+    element.setAttribute("id", "item_in_list");
     element.innerText = item.value;
+    let element_date = document.createElement('div')
+    element_date.setAttribute("id", "date_in_list");
+    element_date.innerText = date.value;
     let to_do_list = document.getElementById('todo_list');
     to_do_list.appendChild(element);
+    let to_do_list_date = document.getElementById('todo_list_date');
+    to_do_list_date.appendChild(element_date);
     document.getElementById('new_item').value = "";
-    console.log(allItems);
+
+
 
 
 }
